1c9e19a1d1396bcb7f370261fca5ee15cdbe2b18
Rex Oliver <rexoliver12@gmail.com>
2023-03-24 10:23:57 -0400

    scaffolding added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import About from './components/About';
+import Contact from './components/Contact';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1>Test</h1>
-    </div>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Projects} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Layout>
+    </Router>
   );
-}
-
+};
 export default App;
---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="header__logo">Your Name</div>
+      <nav className="header__nav">
+        <Link to="/">Projects</Link>
+        <Link to="/resume">Resume</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
