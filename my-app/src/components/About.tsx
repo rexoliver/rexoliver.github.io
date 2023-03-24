43fbcaad862c5356d029fe78fd8a78e4cb1ac884
Rex Oliver <rexoliver12@gmail.com>
2023-03-24 11:04:27 -0400

    created scaffolding

---
+++ my-app/src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About: React.FC = () => {
+    return (
+        <>
+            <h1>About Section</h1>
+        </>
+    );
+}
+
+export default About;
---
+++ my-app/src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Contact: React.FC = () => {
+    return (
+        <>
+            <h1>About Section</h1>
+        </>
+    );
+}
+
+export default Contact;
