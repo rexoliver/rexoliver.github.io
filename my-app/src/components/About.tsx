e600c95c6abfa1aa6e5de13e5ae49d2e871a20d8
Rex Oliver <rexoliver12@gmail.com>
2023-03-28 12:29:20 -0400

    prelim contact and about

--- my-app/src/components/About.tsx
+++ my-app/src/components/About.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import '../App.css';
 
-const About: React.FC = () => {
-    return (
-        <>
-            <h1>About Section</h1>
-        </>
-    );
-}
+const About = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>About Me</h1>
+      </header>
+      <div className="main">
+        <h2>Your Name</h2>
+        <p>I am a Full Stack Developer with 2 years of experience, skilled in multiple programming languages and frameworks. I'm passionate about learning new technologies and building user-friendly applications.</p>
+        <h3>Contact Information:</h3>
+        <ul>
+          <li>Phone: (123) 456-7890</li>
+          <li>Email: <a className="App-link" href="mailto:youremail@example.com">youremail@example.com</a></li>
+          <li>GitHub: <a className="App-link" href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">github.com/yourusername</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- my-app/src/components/Contact.tsx
+++ my-app/src/components/Contact.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Contact: React.FC = () => {
+const Contact = () => {
     return (
-        <>
-            <h1>Contact Section</h1>
-        </>
+      <div className="App">
+        <header className="App-header">
+          <h1>Contact Me</h1>
+        </header>
+        <div className="main">
+          <h2>Your Name</h2>
+          <p>I would love to hear from you! Feel free to reach out using any of the contact methods below:</p>
+          <ul>
+            <li>Phone: (123) 456-7890</li>
+            <li>Email: <a className="App-link" href="mailto:youremail@example.com">youremail@example.com</a></li>
+            <li>GitHub: <a className="App-link" href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">github.com/yourusername</a></li>
+          </ul>
+        </div>
+      </div>
     );
-}
-
-export default Contact;
+  };
+  
+  export default Contact;
